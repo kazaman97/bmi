9592fdd7be253862f7e672c94b2b3ec5fa2d5755
narusai <narusai97@gmail.com>
2017-03-16 12:40:01 +0900

    add webView.

--- bmi/SecondViewController.swift
+++ bmi/SecondViewController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UIViewController, UIWebViewDelegate {
+    @IBOutlet weak var webView: UIWebView!
+    
+    let url: URL = URL(string: "https://ja.wikipedia.org/wiki/%E3%83%9C%E3%83%87%E3%82%A3%E3%83%9E%E3%82%B9%E6%8C%87%E6%95%B0")!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // webViewデリゲートを設定
+        self.webView.delegate = self
+        
+        // URL読み込み
+        let request: NSURLRequest = NSURLRequest(url: url)
+        webView.loadRequest(request as URLRequest)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        print("webViewDidFinishLoad")
+    }
+    
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        print("webViewDidStartLoad")
+    }
+    
 }
 
